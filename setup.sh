4ab5b6493e93a88b6ab6a6a93412e5a2eca43408
Mostafa Shuva <mshuva@umich.edu>
2023-01-07 17:26:35 -0500

    updates for zsh plugins

--- setup.sh
+++ setup.sh
@@ -12,3 +12,20 @@ sudo apt-get update
 echo "Installing following packages"
 echo $PACKAGES
 sudo apt-get install -y $PACKAGES
+
+echo "Installing Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing Zsh Autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Installing Zsh Syntax Highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Copying files..."
+cp .bashrc ~/
+echo ".bashrc -> ~/ ...DONE"
+cp .zshrc ~/
+echo ".zshrc -> ~/ ...DONE"
+cp neofetch/* ~/.config/neofetch/
+echo "neofetch/* -> ~/config/neofetch/ ...DONE"
