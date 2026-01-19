75ec4781f46c0552710842da5cb8c757d06236d7
Mostafa Shuva <mshuva@umich.edu>
2026-01-19 11:53:19 -0500

    Made changes specific to Debian 13 installation

--- setup_new.sh
+++ setup_new.sh
@@ -14,22 +14,27 @@ fi
 export PKGS="zsh fastfetch btop neovim curl wget git codium"
 
 # OS-Specific unique packages
-export DEBIAN_PKGS="chsh python3-venv power-profiles-daemon xserver-xorg-input-synaptics"
+# Supports Debian 13+, Ubuntu 24.02+
+export DEBIAN_PKGS="ca-certificates python3-venv power-profiles-daemon vlc"
 export UBUNTU_PKGS="python3-venv vlc"
 export FEDORA_PKGS="util-linux-user gvfs-mtp simple-mtpfs"
 
 # 3. Installation Logic
 if [ "$OS_ID" = "debian" ] || [ "$OS_ID" = "ubuntu" ]; then
     echo "Setting up VSCodium repo..."
-    wget -qO - gitlab.com | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-    echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] download.vscodium.com vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
-    
+    wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+    echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.sources
+
     sudo apt-get update
     # Installs PKGS + OS-specific packages
     if [ "$OS_ID" = "debian" ]; then
-        sudo apt-get install -y "$PKGS" "$DEBIAN_PKGS"
+        sudo apt-get install -y $PKGS $DEBIAN_PKGS
     else
-        sudo apt-get install -y "$PKGS" "$UBUNTU_PKGS"
+        sudo apt-get install -y $PKGS $UBUNTU_PKGS
     fi
 
 elif [ "$OS_ID" = "fedora" ]; then
@@ -38,12 +43,19 @@ elif [ "$OS_ID" = "fedora" ]; then
     printf "[vscodium]\nname=vscodium\nbaseurl=download.vscodium.com\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=gitlab.com" | sudo tee /etc/yum.repos.d/vscodium.repo
     
     # Installs PKGS + Fedora-specific packages
-    sudo dnf install -y "$PKGS" "$FEDORA_PKGS"
+    sudo dnf install -y $PKGS $FEDORA_PKGS
 fi
 
 # 4. Post-Install Tools (Unattended)
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL raw.githubusercontent.com)" "" --unattended
+echo "Installing Oh My Zsh (unattended)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo "Installing Zsh Autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Installing Zsh Syntax Highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 
 # 5. Shell Configuration
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.backup
